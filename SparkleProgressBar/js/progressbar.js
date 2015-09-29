c13d8978dcf139fc07039434988b5b56c19d2fc8
Hsuching <martin.hsuching@gmail.com>
2015-09-29 16:58:15 +0800

    add

--- SparkleProgressBar/js/progressbar.js
+++ SparkleProgressBar/js/progressbar.js
@@ -3,7 +3,7 @@
  */
 $(function () {
 
-	var endPer=$('.progressbar').attr('data-perc');
+	var endPer = $('.progressbar').attr('data-perc');
 	$('.progressbar').each(function () {
 		var t = $(this),
 			dataperc = t.attr('data-perc'),
@@ -31,12 +31,15 @@ $(function () {
 		var b_c_e_2 = 0;
 		var a_c_e_2 = 1;
 
-		function perc() {
+		function perChange() {
 			var length = t.find('.bar').css('width'),
 				perc = Math.round(parseInt(length) / 5.56),
 				labelpos = (parseInt(length) - 2);
-			if(perc>=endPer){
+			if (perc >= endPer) {
+				//window.cancelAnimationFrame(requestFn);
 				clearInterval(interval);
+			} else {
+				//requestFn(perChange);
 			}
 			var rgba_s = "rgba(" + (r_c_s - Math.round(perc * (r_c_s - r_c_e) / 100)) + "," + (g_c_s + Math.round(perc * (g_c_e - g_c_s) / 100)) + "," + (b_c_s - Math.round(perc * (b_c_s - b_c_e) / 100)) + "," + ".8" + ")";
 			var rgba_e = "rgba(" + (r_c_s_2 - Math.round(perc * (r_c_s_2 - r_c_e_2) / 100)) + "," + (g_c_s_2 + Math.round(perc * (g_c_e_2 - g_c_s_2) / 100)) + "," + (b_c_s_2 - Math.round(perc * (b_c_s_2 - b_c_e_2) / 100)) + "," + a_c_s_2 + ")";
@@ -46,9 +49,19 @@ $(function () {
 			t.find('.label').css('background', "linear-gradient(to bottom, " + rgba_s + " 0%," + rgba_e + " 100%)");
 			t.find('.label').css('box-shadow', "0px 0px 10px 2px " + rgba_s + ",inset 0px 1px 0px 0px rgba(255, 255, 255, 0.45),inset 1px 0px 0px 0px rgba(255, 255, 255, 0.25),inset -1px 0px 0px 0px rgba(255, 255, 255, 0.25)");
 			t.find('.perc').text(perc + '%');
+
 		}
 
-		perc();
-		var interval=setInterval(perc, 0);
+		//perc();
+		var requestFn = (function () {
+			return window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				function (callback) {
+					window.setTimeout(callback, 0);
+				};
+		})();
+		//requestFn(perChange);
+		var interval=setInterval(perChange, 0);
 	});
 });
